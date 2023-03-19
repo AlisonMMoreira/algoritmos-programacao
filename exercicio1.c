bd289d147ef02a526479b0757fdb76d83d2e15e0
Alison Moreira <120423515+AlisonMMoreira@users.noreply.github.com>
2023-03-18 22:19:44 -0300

    Create exercicio1.c

---
+++ exercicio1.c
@@ -0,0 +1,23 @@
+//Faça um programa em C que leia dois valores e faça as quatro operaçãoes aritméticas básicas (+,-,*,/) com os dois valores, mostrando os resultados.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <locale.h>
+
+int main(){
+	int num1, num2, soma, subtracao, multiplicacao, divisao;
+	setlocale(LC_ALL,"Portuguese");
+	printf("Digite o primeiro valor: ");
+	scanf("%i", &num1);
+	printf("Digite o segundo valor: ");
+	scanf("%i", &num2);
+	soma = num1 + num2;
+	subtracao = num1 - num2;
+	multiplicacao = num1 * num2;
+	divisao = num1 / num2;
+	printf("Se somados darão o seguinte resultado: %i\n", soma);	
+	printf("Se darão o seguinte resultado: %i\n", subtracao);
+	printf("Se multiplicados darão: %i\n", multiplicacao);
+	printf("Se divisao darão: %i\n", divisao);
+}
