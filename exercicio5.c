2ecd5a5c21218588035ed49ad41e89b8f31daeba
Alison Moreira <120423515+AlisonMMoreira@users.noreply.github.com>
2023-03-19 23:23:26 -0300

    Create exercicio5.c

---
+++ exercicio5.c
@@ -0,0 +1,17 @@
+//Desenvolva um programa em C que receba o preço de um produto e o percentual de desconto, calcule e mostre o valor do desconto e o novo preço do produto.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <locale.h>
+
+int main(){
+	float p_produto, p_desconto, desconto, n_p;
+	setlocale(LC_ALL, "Portuguese");
+	printf("Digite o valor do produto escolhido: ");
+	scanf("%f", &p_produto);
+	printf("Digite o percentual de desconto: ");
+	scanf("%f", &p_desconto);
+	n_p = (p_produto * p_desconto) / 100;
+	printf("O valor total a pagar é de: %.2f: ", n_p);
+}
