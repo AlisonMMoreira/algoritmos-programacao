f2383b13daf34415ed830501dfae81dd1523b34f
AlisonMMoreira <alison.moreira2302@gmail.com>
2023-04-17 23:56:40 -0300

    organizando os arquivos

---
+++ exemplos/exemplo0.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <locale.h>
+int main()
+{
+	float n,y,potenciacao;
+	setlocale(LC_ALL,"Portuguese");
+	
+	printf("Digite um numero: ");
+	scanf("%f",&n);
+	printf("Digite o número de elevacao: ");
+	scanf("%f",&y);
+	potenciacao = pow(n,y);
+	printf("O resultado é: %f", potenciacao, "\n");
+	system("pause");
+	return 0;
+}
