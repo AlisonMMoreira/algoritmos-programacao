ed53e42239caaee8a48b31ee11fbb0b4432699c1
AlisonMMoreira <alison.moreira2302@gmail.com>
2023-04-17 23:59:26 -0300

    Exercícios de Estruturas Seletivas

---
+++ estruturas seletivas/exercicio1.c
@@ -0,0 +1,22 @@
+#include <locale.h>
+#include <stdio.h>
+
+void main() {
+  setlocale(LC_ALL, "Portuguese");
+  float n1, n2, n3;
+
+  printf("Digite três valores: ");
+  scanf("%f", &n1);
+  scanf("%f", &n2);
+  scanf("%f", &n3);
+
+  if (n1 > n2 && n1 > n3) {
+    printf("%.2f é maior!", n1);
+  }
+  if (n1 < n2 && n2 > n3) {
+    printf("%.2f é maoir!", n2);
+  }
+  if (n1 < n2 && n2 < n3) {
+    printf("%.2f é maior!", n3);
+  }
+}
