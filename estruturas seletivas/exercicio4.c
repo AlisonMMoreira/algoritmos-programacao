bd5bb0a1fbfa52d52c79e9d59360f2403118f422
AlisonMMoreira <alison.moreira2302@gmail.com>
2023-05-21 17:56:09 -0300

    exercícios

---
+++ estruturas seletivas/exercicio4.c
@@ -0,0 +1,20 @@
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  setlocale(LC_ALL, "Portuguese");
+  int ano;
+  printf("Digite um ano: ");
+  scanf("%d", &ano);
+  if ((ano % 4 == 0 && ano % 100 != 0) || ano % 400 == 0) {
+    printf("%d é um ano bissexto.\n", ano);
+  } else {
+    printf("%d não é ano bissexto.\n", ano);
+  }
+  return 0;
+}
+
+
+
+
