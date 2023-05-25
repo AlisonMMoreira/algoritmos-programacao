d9b589b48d0bf34d05005be87859d41ce9653395
Alison Moreira <120423515+AlisonMMoreira@users.noreply.github.com>
2023-05-24 23:16:27 -0300

    Update exemplo4.c

--- exercicios_com_vetores/exemplo4.c
+++ exercicios_com_vetores/exemplo4.c
@@ -1,1 +1,28 @@
-/*Crie um programa com uma função que receba um vetor e o seu tamanho por parâmetro e retorne a média dos elementos do vetor para o programa principal.*/
+/*
+Crie um programa com uma função que receba um vetor e o seu tamanho por parâmetro e retorne a média dos elementos do vetor para o programa principal.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#define N 5
+
+float funcao( int a[], int size )
+{
+  int i;
+  float soma = 0;
+  
+  for ( i = 0; i < size; i++ )
+    soma += a[i];
+  return( soma / size );
+}
+int main()
+{
+  int a[N];
+  int i;
+  
+  for ( i = 0; i < N; i++ )
+    a[i] = i;
+  printf("Resultado = %5.2f\n", funcao(a, N));
+  system ("pause");
+  return 0;
+}
